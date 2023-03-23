d2235ff15c233f8472f37d64dfcbbf13d661f8de
Frank Kachinga <frank.kachinga@gmail.com>
2023-03-23 16:02:16 +0300

    secret manager

--- DTOs/CreateItemDto.cs
+++ DTOs/CreateItemDto.cs
@@ -4,7 +4,10 @@ namespace SuperHeroAPI.DTOs;
 
 public class CreateItemDto
 {
-    [Required] public string Name { get; init; } = string.Empty;
+    [Required] 
+    public string Name { get; init; } = string.Empty;
 
-    [Required] [Range(0, 100000)] public decimal Price { get; init; }
+    [Required] 
+    [Range(0, 100000)] 
+    public decimal Price { get; init; }
 }
--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -4,6 +4,9 @@ public class MongoDbSettings
 {
     public string Host { get; set; }
     public int Port { get; set; }
+    
+    public string User { get; set; }
+    public string Password { get; set; }
 
-    public string connectionString => $"mongodb://{Host}:{Port}";
+    public string connectionString => $"mongodb://{User}:{Password}@{Host}:{Port}";
 }
